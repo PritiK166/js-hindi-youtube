2b50fec8ad6af35405f3bae695037383388e9fe9
PritiK166 <pkandalkar16@gmail.com>
2025-07-18 11:47:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"  // treat all js code as newer version
+
+// alert (3+3) We are using node js not browser
+
+// console.log(3 
+//     + 
+//     3)  // code readability should be high
+
+
+// console.log("hii");
+
+
+let name = "hela"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 24
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value 
+// undefined =>
+// symbol => unique 
+
+// objects
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)    // object
